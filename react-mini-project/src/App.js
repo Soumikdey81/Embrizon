aa6feb4bacf19ba474280c720485c6174e14ec32
Soumikdey81 <aditdey30@gmail.com>
2025-08-10 19:56:47 +0530

    react

--- react-mini-project/src/App.js
+++ react-mini-project/src/App.js
@@ -3,24 +3,24 @@ import CardList from "./components/CardList";
 import Header from "./components/Header";
 
 export default function App() {
-  const BASE_URL = "http://localhost:8000";
-
   const [apps, setApps] = useState([]);
 
   useEffect(function () {
     async function fetchApps() {
       try {
-        const res = await fetch(`${BASE_URL}/apps`);
+        const res = await fetch("/apps.json");
         const data = await res.json();
-        setApps(data);
+        setApps(data.apps);
       } catch {
         alert("There was an error loading data...");
       }
     }
     fetchApps();
   }, []);
+
   return (
     <>
+      {console.log(apps)}
       <Header />
       <CardList apps={apps} />
     </>
